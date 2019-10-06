fa2e93a924b2977893b45ed800d3a9064b619174
eg7eg7 <eg7eg7@gmail.com>
2019-10-06 04:53:47 +0300

    improvements

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,23 +1,53 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Stream;
 
 public class Database {
 	private static ArrayList<Programming> programmingList = new ArrayList<Programming>();
 	private static String globeFile = "Autoglobe.dat";
-
-	private File f;
-
+	
 	public Database() {
-		fillDatabaseFromFile(new File(globeFile));
 	}
 
-	private void fillDatabaseFromFile(File f) {
+	public void fillDatabaseFromFile() {
 		// TODO clean previous data and read again
 		// TODO Get file location here
+		programmingList.clear();
+		int num_programs = 0;
+		
+		ObjectInputStream oos = null;
+		try {
+			FileInputStream fos = new FileInputStream(globeFile);
+			oos = new ObjectInputStream(fos);
 
+			num_programs = oos.readInt();
+			for(int i=0;i<num_programs;i++)
+			{
+				try {
+					addProgramming((Programming) oos.readObject());
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+				
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(oos !=null)
+				try {
+					oos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
 	}
 
 	public static ArrayList<Programming> getProgrammingList() {
@@ -30,30 +60,45 @@ public class Database {
 
 	public void addProgramming(Programming p) {
 		if (!programmingList.contains(p))
+		{
 			programmingList.add(p);
+		}
 	}
 
 	@Override
 	public String toString() {
-		return programmingList.toString();
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0;i<programmingList.size();i++)
+		{
+			sb.append(programmingList.get(i).toString());
+			sb.append(showNewMovies(i));
+			sb.append(showExpiredMovies(i));
+		}
+		return sb.toString();
 	}
 
-	public void showExpiredMovies(int i) {
+	public String showExpiredMovies(int i) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\nExpired movies :\n");
 		if(i-2 < 0)
 		{
 
-			System.out.println("No expired movies");
-			return;
+			sb.append("No expired movies");
+			return sb.toString();
 		}
-
+		else if (i>programmingList.size())
+		{
+			sb.append("Invalid programming value");
+			return sb.toString();
+		}
 		Set<String> old_movies = programmingList.get(i-2).getMovie_titles();
 		Set<String> new_movies = programmingList.get(i-1).getMovie_titles();
 		Set<String> removed_movies = new HashSet<String>();
-		
-		
+
+
 		String[] old_movies_arr = old_movies.stream().toArray(n -> new String[n]);
-		
-		
+
+
 		for(int j=0;j<old_movies_arr.length;j++)
 		{
 			String movie_name = old_movies_arr[j];
@@ -63,8 +108,69 @@ public class Database {
 			}
 		}
 		
-		System.out.println(removed_movies);
-		
+		for(String s : removed_movies)
+			sb.append(s + "\n");
+		return sb.toString();
 	}
+	
+	public String showNewMovies(int i) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\nNew movies :\n");
+		if(i-2 < 0)
+		{
+
+			sb.append("No new movies");
+			return sb.toString();
+		}
+		else if (i>programmingList.size())
+		{
+			sb.append("Invalid programming value");
+			return sb.toString();
+		}
+		Set<String> old_movies = programmingList.get(i-2).getMovie_titles();
+		Set<String> new_movies = programmingList.get(i-1).getMovie_titles();
+		Set<String> brand_new = new HashSet<String>();
+
+
+		String[] new_movies_arr = new_movies.stream().toArray(n -> new String[n]);
+
+
+		for(int j=0;j<new_movies_arr.length;j++)
+		{
+			String movie_name = new_movies_arr[j];
+			if(!old_movies.contains(movie_name))
+			{
+				brand_new.add(movie_name);
+			}
+		}
+		
+		for(String s : brand_new)
+			sb.append(s + "\n");
+		return sb.toString();
+	}
+
+	public void saveToFile() {
+		File file = new File(globeFile);
+		ObjectOutputStream oos = null;
+		try {
+			FileOutputStream fos = new FileOutputStream(file);
+			oos = new ObjectOutputStream(fos);
+			oos.writeInt(programmingList.size());
+			for(int i=0;i<programmingList.size();i++)
+				oos.writeObject(programmingList.get(i));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(oos !=null)
+				try {
+					oos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+	}
+
 
 }
--- src/main/java/Programming.java
+++ src/main/java/Programming.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -27,17 +29,19 @@ public class Programming implements Serializable {
 	private final static int READ_MOVIES = 3;
 
 	private static final long serialVersionUID = 1L;
-	Set<String> movie_titles;
+	private Set<String> movie_titles;
 	// Set of movies for the week
-	String Title;
-	Map<Integer, Set<Projection>> projections;
-
+	private String Title;
+	private Map<Integer, Set<Projection>> projections;
+	private Map<Integer, Set<String>> movies_hall;
+	private String filename;
 	private int state = BEGIN;
 
 	public Programming() {
 		movie_titles = new HashSet<String>();
 		// movie_hall_movies = new HashMap<Integer, Map<String,Set<Integer>>>();
 		projections = new HashMap<Integer, Set<Projection>>();
+		movies_hall = new HashMap<Integer, Set<String>>();
 	}
 
 	public Set<String> getMovie_titles() {
@@ -71,19 +75,35 @@ public class Programming implements Serializable {
 	public Programming(String xlsFilePath) {
 		this();
 		readExcel(xlsFilePath);
-		
+		int num = 0;
+		for(int i=1;i<=main.getMAX_HALL();i++)
+		{
+			if(projections.containsKey(i))
+			{
+				for(Projection p : projections.get(i))
+				{
+					if(movies_hall.get(i) == null)
+						movies_hall.put(i, new HashSet<String>());
+					movies_hall.get(i).add(p.getName());
+				}
+			}
+		}
 	}
 
 	private void readExcel(String xlsFilePath) {
+		setFilename(xlsFilePath);
+		int count=0;
 		String date = "";
 		String hall = "";
 		String name;
 		String start_time;
 		String break_time;
 		String end_time;
+		File file;
 		try {
-
-			FileInputStream inputStream = new FileInputStream(new File(xlsFilePath));
+			file = new File(xlsFilePath);
+			System.out.println("Reading from : " + file.getAbsoluteFile());
+			FileInputStream inputStream = new FileInputStream(file);
 			main.log("reading " + xlsFilePath);
 			Workbook workbook = getRelevantWorkbook(inputStream, xlsFilePath);
 			Sheet sheet = workbook.getSheetAt(main.getCHOSEN_SHEET());
@@ -94,7 +114,7 @@ public class Programming implements Serializable {
 			Row row2;
 			Cell cell1;
 			Cell cell2;
-
+			String value=null;
 			while (iterator.hasNext()) {
 				row1 = iterator.next();
 				cellIterator = row1.cellIterator();
@@ -112,67 +132,86 @@ public class Programming implements Serializable {
 					date = header[header.length - 1];
 					main.log("Read date: " + date + " at " + cell1.getAddress());
 					state = HALL_NUM;
-					row1 = iterator.next();// skip "Ulam"
+					row1 = iterator.next(); // skip "Ulam" row
 					
 					break;
 				case HALL_NUM:
 					cell1 = cellIterator.next();
 					hall = cell1.getStringCellValue();
 					main.log("Read hall num: " + hall + " at " + cell1.getAddress());
+					try {
+						Integer.parseInt(hall);
+					}
+					catch(NumberFormatException e)
+					{
+						state = READ_DATE;
+						break;
+					}
 					state = READ_MOVIES;
 				case READ_MOVIES:
 					cell1 = cellIterator.next();// hour viewer
 					row2 = iterator.next();
-					cellIterator2 = row2.cellIterator();
+					cellIterator2 = row2.cellIterator(); //title viewer
+
+					cell2 = cellIterator2.next();//movie viewer
 					while (cellIterator2.hasNext()) {
-
 						cell2 = cellIterator2.next();//movie viewer
-						String value = cell2.getStringCellValue();
-
+						value = cell2.getStringCellValue();
+						main.log("trying to read " + value);
 						if (!value.equals("")) {
 						
 							main.log("Read movie " + value + " at " + cell2.getAddress());
 							name = value;
 							start_time = cell1.getStringCellValue();
 							main.log("Read time " + start_time + " at " + cell1.getAddress());
-							cell1 = cellIterator.next();
+							if (cellIterator.hasNext())
+								cell1 = cellIterator.next();
 							break_time = cell1.getStringCellValue();
 							main.log("Read time " + break_time + " at " + cell1.getAddress());
-							cell1 = cellIterator.next();
+							if (cellIterator.hasNext())
+								cell1 = cellIterator.next();
 							end_time = cell1.getStringCellValue();
 							main.log("Read time " + end_time + " at " + cell1.getAddress());
+							
+							/* Try to read from next cell*/
 							if (cellIterator.hasNext())
 								cell1 = cellIterator.next();
 							if(cellIterator2.hasNext())
 								cell2 = cellIterator2.next();
 							if(cellIterator2.hasNext())
 								cell2 = cellIterator2.next();
+							
 							Projection p = new Projection(name, start_time, break_time, end_time, date);
+							count++;
 							addProjection(Integer.parseInt(hall), p);
 						}
-						else
+						else //empty cell
 						{
 							cell1 = cellIterator.next();
 							cell1 = cellIterator.next();
-							cell1 = cellIterator.next();
+							if(cellIterator.hasNext())
+								cell1 = cellIterator.next();
+							
 
 							if(cellIterator2.hasNext())
 								cell2 = cellIterator2.next();
 							if(cellIterator2.hasNext())
 								cell2 = cellIterator2.next();
-							if(cellIterator2.hasNext())
-								cell2 = cellIterator2.next();
+						
 						}
 					}
 
 					
 					state = HALL_NUM;
 					if(Integer.parseInt(hall) == main.getMAX_HALL())
+					{
+						//row1 = iterator.next();
 						state = READ_DATE;
+					}
 				}
 
 			}
-
+			System.out.println("Finished adding " + count + " projections");
 			workbook.close();
 			inputStream.close();
 		} catch (Exception e) {
@@ -195,7 +234,7 @@ public class Programming implements Serializable {
 			projections.put(hall_num, new HashSet<Projection>());
 		}
 		projections.get(hall_num).add(p);
-		if(p.getType().equals(main.MOVIE))
+		//if(p.getType().equals(main.MOVIE))
 			movie_titles.add(p.getName());
 	}
 
@@ -216,13 +255,40 @@ public class Programming implements Serializable {
 	@Override
 	public String toString() {
 		final StringBuilder b = new StringBuilder();
+		b.append("Printing programming summary for " + filename + "\n");
+		for(int i=1;i<=main.getMAX_HALL();i++)
+		{
+			if(projections.containsKey(i))
+			{
+
+				//b.append("\nHALL " + i + "\n" + projections.get(i));
+				b.append("\n---------------HALL " + i + "-----------------\n");
+				for(String s : movies_hall.get(i))
+				{
+					b.append(s + "\n");
+				}
+			}
+		}
+		
+		movie_titles.forEach(new Consumer<String>() {
+			public void accept(String s) {
+				//b.append(s + "\n");
+			}
+		});
+			
+		
+		return b.toString();
+	}
+	
+	public String prettyPrint() {
+		final StringBuilder b = new StringBuilder();
 		System.out.println(Title);
 		for(int i=1;i<=main.getMAX_HALL();i++)
 		{
 			if(projections.containsKey(i))
 			{
 
-				b.append("\nHALL " + i + "\n" + projections.get(i));
+				b.append("\n---------------HALL " + i + "-----------------\n" + movies_hall.get(i));
 			}
 		}
 		
@@ -235,6 +301,20 @@ public class Programming implements Serializable {
 		
 		return b.toString();
 	}
-	
-	
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public Map<Integer, Set<String>> getMovies_hall() {
+		return movies_hall;
+	}
+
+	public void setMovies_hall(Map<Integer, Set<String>> movies_hall) {
+		this.movies_hall = movies_hall;
+	}
 }
